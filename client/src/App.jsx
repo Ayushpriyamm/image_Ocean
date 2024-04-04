58d351345b5365980f1b8284223cada4a24c9106
Ayush Priyam <ayushpriyam24@gmail.com>
2024-04-04 16:08:55 +0530

    test git working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="text-red-500 text-8xl text-center  items-center">App</div>
+    <div className="text-green-500 text-8xl text-center  items-center">App</div>
   );
 };
 
