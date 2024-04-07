3f11efb859921df06bf6bea3edada94fa63fa2f1
Ayush Priyam <ayushpriyam24@gmail.com>
2024-04-07 12:51:25 +0530

    create route and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
+import { Create } from "./pages/Create";
+import { Explore } from "./pages/Explore";
+import { Signup } from "./pages/Signup";
+import { Login } from "./pages/Login";
 
 const App = () => {
   return (
-    <div className="text-green-500 text-8xl text-center  items-center">App</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <div className=" bg-slate-700">navbar</div>
+    </div>
+  );
+};
