66c9dca1451aabd6073cb55caa764e0d53cb3856
Ayush Priyam <ayushpriyam24@gmail.com>
2024-08-05 11:52:34 +0530

    add preInstallation for deployment

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import imageRouter from './routes/imageRouter.js'
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config()
 
@@ -18,6 +19,8 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 })
 
+const __dirname = path.resolve();
+
 app.listen(3000, () => {
     console.log("server is running on port 3000!!!")
 })
@@ -30,6 +33,12 @@ app.use(cors());
 
 app.use('/server/auth', userRouter)
 app.use('/server/images', imageRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 
 app.use((err, req, res, next) => {
