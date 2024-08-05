a8c20cce95e7329d786f9910fe169d3f88e00266
Ayush Priyam <ayushpriyam24@gmail.com>
2024-08-05 11:41:20 +0530

    submit button functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 
 export const Header = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  //handle change in serach box
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <header className=" bg-[#1A1A1A] shadow-lg  ">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3 ">
@@ -11,13 +22,18 @@ export const Header = () => {
             Image<span className="text-[#008170] ">Ocean</span>
           </h1>
         </Link>
-        <form className=" bg-[#232323] p-3 rounded-lg flex items-center">
+        <form
+          className=" bg-[#232323] p-3 rounded-lg flex items-center"
+          onSubmit={handleSubmit}
+        >
           <input
+            onChange={handleChange}
             type="text"
             placeholder="search..."
+            value={searchTerm}
             className=" bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <button>
+          <button type="submit">
             <FaSearch />
           </button>
         </form>
