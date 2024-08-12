a38c9fa280bde4af3713a7c91f5225227c993cae
Ayush Priyam <ayushpriyam24@gmail.com>
2024-08-12 14:38:49 +0530

    cors chaneed

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(cors({
     origin: "https://imageocean.vercel.app/",
     methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: 'Content-Type,Authorization',
     credentials: true  
 }));
 
