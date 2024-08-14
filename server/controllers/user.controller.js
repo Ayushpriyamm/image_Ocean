ca6010b12b7435adea1d11a4c4b3bf38195c0b0f
Ayush Priyam <ayushpriyam24@gmail.com>
2024-08-14 10:52:18 +0530

    fixed signup error

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -11,12 +11,21 @@ export const test = (req, res) => {
 }
 
 export const signup = async (req, res,next) => {
+   try {
     const { username, email, password } = req.body;
+
+    // Check if all required fields are provided
+    if (!username || !email || !password) {
+         return next(errorHandler(400, 'Username, email, and password are required'));
+    }
+    
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
-    const newUser = new User({ username, email, password:hashedPassword });
+    const newUser = new User({ username, email, password: hashedPassword });
+    
+    
 
-    try {
+    
         await newUser.save();
         res.status(201).json("user created successfully")
     } catch (error) {
